35d2518a0f47b0a37950fa5d1822ef1922b2ac9b
davidshare <davidessienshare@gmail.com>
2018-12-31 16:13:27 +0100

    feat(burgerControls): add controls for adding and removing ingredients
    - add a component for displaying each burger control
    - add a component for displaying the burger controls
    - add styles for the burger controls
    - add methods for adding and removing ingredients
    - add functonality for calculating and displaying the burger price

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import '../../css/Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-	const transformedIngredients = Object.keys(props.ingredients)
+	let transformedIngredients = Object.keys(props.ingredients)
 		.map( ingredientKey=>{
 			return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
 				return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
 			})
-		});
+		}).reduce((total, current) => {
+			return total.concat(current)
+		}, []);
+		if(transformedIngredients.length === 0 ){
+			transformedIngredients = <p>Please start adding ingredients!</p>
+		}
 	return (
 		<div className="Burger">
 			<BurgerIngredient type="bread-top"/>
